646d73c371dce42cf1232b35e298b8cdb35f7fc7
Rohit-GitHUb24 <rohitsethi.mca@gmail.com>
2024-09-11 11:33:50 +0530

    updated headless true

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://fr02834vmx.fr.ge.conti.de/home",
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     colorScheme: "dark",
